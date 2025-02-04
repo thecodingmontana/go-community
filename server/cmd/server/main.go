1144d27744cccd2b88e5f66d85268190a3d62619
thecodingmontana <dev.humblechris254@gmail.com>
2025-02-04 13:00:11 +0300

    feat(app): add OAUTH Providers support

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -8,7 +8,12 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth/v5"
+	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
+	"github.com/markbates/goth/providers/google"
 	"github.com/thecodingmontana/go-community/internal/database/models"
 	"github.com/thecodingmontana/go-community/internal/routes"
 	"github.com/thecodingmontana/go-community/pkg/database"
@@ -32,6 +37,13 @@ func init() {
 func main() {
 	PORT, isPortEnv := os.LookupEnv("PORT")
 	DATABASE_URL, isDBURLEnv := os.LookupEnv("DATABASE_URL")
+	OAUTH_GOOGLE_CLIENT_ID, googleClientIDFound := os.LookupEnv("OAUTH_GOOGLE_CLIENT_ID")
+	OAUTH_GOOGLE_CLIENT_SECRET, googleClientSecretFound := os.LookupEnv("OAUTH_GOOGLE_CLIENT_SECRET")
+	OAUTH_GOOGLE_REDIRECT_URI, googleRedirectURIFound := os.LookupEnv("OAUTH_GOOGLE_REDIRECT_URI")
+	OAUTH_GITHUB_CLIENT_ID, githubClientIDFound := os.LookupEnv("OAUTH_GITHUB_CLIENT_ID")
+	OAUTH_GITHUB_CLIENT_SECRET, githubClientIDFound := os.LookupEnv("OAUTH_GITHUB_CLIENT_SECRET")
+	OAUTH_GITHUB_REDIRECT_URI, githubRedirectURIFound := os.LookupEnv("OAUTH_GITHUB_REDIRECT_URI")
+	SESSION_SECRET, isSessionSecretFound := os.LookupEnv("SESSION_SECRET")
 
 	if !isPortEnv {
 		log.Fatalf("Missing PORT env variable!")
@@ -41,11 +53,68 @@ func main() {
 		log.Fatalf("Missing DATABASE_URL env variable!")
 	}
 
+	if !googleClientIDFound {
+		log.Fatalf("Missing OAUTH_GOOGLE_CLIENT_ID env variable!")
+	}
+
+	if !googleClientSecretFound {
+		log.Fatalf("Missing OAUTH_GOOGLE_CLIENT_SECRET env variable!")
+	}
+
+	if !googleRedirectURIFound {
+		log.Fatalf("Missing OAUTH_GOOGLE_REDIRECT_URI env variable!")
+	}
+
+	if !githubClientIDFound {
+		log.Fatalf("Missing OAUTH_GITHUB_CLIENT_ID env variable!")
+	}
+
+	if !githubClientIDFound {
+		log.Fatalf("Missing OAUTH_GITHUB_CLIENT_SECRET env variable!")
+	}
+
+	if !githubRedirectURIFound {
+		log.Fatalf("Missing OAUTH_GITHUB_REDIRECT_URI env variable!")
+	}
+
+	if !isSessionSecretFound {
+		log.Fatalf("Missing SESSION_SECRET env variable!")
+	}
+
 	// Database connection
 	conn := database.ConnectDB(DATABASE_URL)
 	queries := models.New(conn)
 
 	router := chi.NewRouter()
+
+	maxAge := 86400 * 30 // 30 days
+	isProd := true       // Set to true when serving over https
+
+	store := sessions.NewCookieStore([]byte(SESSION_SECRET))
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true
+	store.Options.Secure = isProd
+
+	gothic.Store = store
+
+	// Initialize Goth providers
+	goth.UseProviders(
+		google.New(
+			OAUTH_GOOGLE_CLIENT_ID,
+			OAUTH_GOOGLE_CLIENT_SECRET,
+			OAUTH_GOOGLE_REDIRECT_URI,
+			"email",
+			"profile",
+		),
+		github.New(
+			OAUTH_GITHUB_CLIENT_ID,
+			OAUTH_GITHUB_CLIENT_SECRET,
+			OAUTH_GITHUB_REDIRECT_URI,
+			"user:email",
+			"user",
+		),
+	)
 
 	// Middlewares
 	router.Use(middleware.Logger)
--- server/go.mod
+++ server/go.mod
@@ -5,14 +5,22 @@ go 1.23.5
 require (
 	github.com/go-chi/chi/v5 v5.2.0
 	github.com/go-chi/jwtauth/v5 v5.3.2
+	github.com/gorilla/sessions v1.1.1
 	github.com/gorilla/websocket v1.5.3
 	github.com/jackc/pgx/v5 v5.7.2
 	github.com/joho/godotenv v1.5.1
+	github.com/markbates/goth v1.80.0
 )
 
 require (
+	cloud.google.com/go/compute v1.20.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
@@ -24,7 +32,10 @@ require (
 	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/oauth2 v0.17.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
